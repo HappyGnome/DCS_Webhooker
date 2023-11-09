4d6baeb63152cfceda7406f8c3b3dd8e1968517f
HappyGnome <46838482+HappyGnome@users.noreply.github.com>
2023-11-09 22:07:13 +0000

    Logos added

--- SavedGamesDCS/Mods/Services/DCS_Webhooker/entry.lua
+++ SavedGamesDCS/Mods/Services/DCS_Webhooker/entry.lua
@@ -1,10 +1,10 @@
-declare_plugin("DCS_Webhooker", {
+declare_plugin("Webhooker for DCS by HappyGnome", {
 	installed = true,
 	dirName = current_mod_path,
 	developerName = _("HappyGnome"),
 	developerLink = _("https://github.com/HappyGnome/DCS_Webhooker"),
-	displayName = _("DCS Webhooker"),
-	version = "1.0.0.0",
+	displayName = _("Webhooker"),
+	version = "1.0.0",
 	state = "installed",
 	info = _(""),
 	binaries = {},
@@ -12,6 +12,10 @@ declare_plugin("DCS_Webhooker", {
 	Options = {
 		{ name = "DCS_Webhooker", nameId = "DCS_Webhooker", dir = "options", allow_in_simulation = true; },
 	},
+	Skins = {
+		{ name = "DCS_Webhooker", dir = "theme" },
+	},
+	creditsFile = "credits.txt";
 })
 
 plugin_done()
